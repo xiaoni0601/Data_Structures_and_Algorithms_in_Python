f533748b4b54657e21100202491436032b702b7f
xiaoni0601 <53635574+xiaoni0601@users.noreply.github.com>
2019-12-31 11:44:08 +0800

    turtle

---
+++ MoveTower30.py
@@ -0,0 +1,9 @@
+#问题分析过程
+def moveTower(height, fromPole,withPole,toPole):
+    if height >= 1:
+        moveTower(height - 1, fromPole, toPole, withPole)
+        moveDisk(height,fromPole,toPole)
+        moveTower(height - 1, withPole, fromPole, toPole)
+def moveDisk(disk,fromPole, toPole):
+    print(f"moving disk[{disk}] from {fromPole} to {toPole}")
+moveTower(3,"#1","#2","#3")
